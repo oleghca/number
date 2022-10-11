97c3ed0bbcc5fa9f832421a6924be08d754142c1
oleghca <108630955+oleghca@users.noreply.github.com>
2022-10-11 14:32:54 +0300

    upload site

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,22 @@
-let correctNumber = "3463463460";
+let correctNumber ="2845718432";
  console.log(correctNumber);
 
  let d=correctNumber.split('');
- d=(((d[0]*1 + d[1]*5 + d[2]*7 + d[3]*9 + d[4]*4
- + d[5]*6 + d[6]*10 + d[7]*5 + d[8]*7)%11)%10)
+ d=((d[0]*2 + d[1]*4 + d[2]*10 + d[3]*3 + d[4]*5
+ + d[5]*9 + d[6]*4 + d[7]*6 + d[8]*8))
   console.log(d);
+  let af = d%11
+console.log(af)
+let n = "жен"; 
+if((correctNumber[8]%2) === 0){
+console.log(n)
+}else{console.log('муж')}
+
 
  let newCode = correctNumber.substring(0,5);
-   let birth = new Date( 1899,12,31, 0, 0);
-    birth.setDate(newCode);
-    console.log(`Дата рождения:${birth}`);
+   let birt= new Date( 1899,12,31, 0, 0);
+    birt.setDate(newCode);
+    console.log(`Дата рождения:${birt}`);
     let yr =new Date()
-    let s = (yr-birth)/1000/60/60/24/365
+    let s = (yr-birt)/1000/60/60/24/365
     console.log(`Полных лет:${ Math.floor(s)}`)
-
-  
