c1d24e0fa3c4129a450a26619935a41ec6580d73
oleghca <108630955+oleghca@users.noreply.github.com>
2022-10-17 16:32:56 +0300

    upload site

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,46 @@
-let correctNumber ="2845718432";
- console.log(correctNumber);
-
+ 
+ 
+ 
+ 
+ let correctNumber ="3463463460";
+ function lk(correctNumber){
  let d=correctNumber.split('');
- d=((d[0]*2 + d[1]*4 + d[2]*10 + d[3]*3 + d[4]*5
- + d[5]*9 + d[6]*4 + d[7]*6 + d[8]*8))
-  console.log(d);
-  let af = d%11
-console.log(af)
-let n = "жен"; 
+ d=(d[0]*(-1) + d[1]*5 + d[2]*7 + d[3]*9 + d[4]*4
+ + d[5]*6 + d[6]*10 + d[7]*5 + d[8]*7)%11
+  return d;}
+let b=lk(correctNumber)
+console.log(b)
+if(b===10){
+  b=b%10
+}
+if(b!=correctNumber[9]){
+
+
+  console.log('введите правильный номер')
+  throw new SyntaxError();
+}
+let newCode = correctNumber.substring(0,5);
+   let birt= new Date( 1899,12,31,);
+   birt.setDate(newCode);
+  console.log(`Дата рождения:${birt}`);
+ let now =new Date();
+ let todey=new Date(now.getFullYear(),now.getMonth(),now.getDate());
+ let kj=new Date(todey.getFullYear(),birt.getMonth(),birt.getDate());
+let age=todey.getFullYear()-birt.getFullYear();
+if(todey<kj){
+  age=age-1;
+  console.log(`Полных лет: ${age}`)
+}
+let n = "Пол жен."; 
 if((correctNumber[8]%2) === 0){
 console.log(n)
-}else{console.log('муж')}
+}else{console.log('Пол муж.')}
 
 
- let newCode = correctNumber.substring(0,5);
-   let birt= new Date( 1899,12,31, 0, 0);
-    birt.setDate(newCode);
-    console.log(`Дата рождения:${birt}`);
-    let yr =new Date()
-    let s = (yr-birt)/1000/60/60/24/365
-    console.log(`Полных лет:${ Math.floor(s)}`)
+
+// let cart="4314140107615251"
+// let cot=cart.split('')
+// console.log(cot)
+// let num ="visa";
+// if(cot[[0]]==4){
+//   console.log(num)}
